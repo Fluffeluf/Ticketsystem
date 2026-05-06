ffbe1a2a8188473522330552574c254366367bc1
Cecilia Gross <fluffeluf@icloud.com>
2026-05-06 20:53:01 +0200

    Added DBZugriff.cs

---
+++ Persistenz/DBZugriff.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Ticketsystem
+{
+    public static class DBZugriff
+    {
+        public static MySqlConnection OpenDB()
+        {
+            string conStr = "Server=use.yourserver.here;Database=your_database;Uid=your_user;Pwd=your_password;";
+            MySqlConnection con = new MySqlConnection(conStr);
+            con.Open();
+            return con;
+        }
+
+        public static void CloseDB(MySqlConnection con)
+        {
+            // Brauchen wir nicht da des mit dem using ja selbst erledigt wird aber ist halt einfach da
+            con.Close();
+        }
+
+        // Wenig nährwert
+        public static int ExecuteNonQuery(string sql)
+        {
+            using MySqlConnection con = OpenDB();
+            MySqlCommand command = new MySqlCommand(sql, con);
+            return command.ExecuteNonQuery();
+        }
+
+        public static MySqlDataReader ExecuteReader(string sql, MySqlConnection con)
+        {
+            MySqlCommand command = new MySqlCommand(sql, con);
+            return command.ExecuteReader();
+        }
+    }
+}
